1537dc830fed87edfbe54cd6224cc6214a64b65a
Aniceto <anibuendia3@gmail.com>
2021-10-20 15:28:44 +0200

    Proyect + TailwindCSS + Cookies.js + images + app.js

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        mediadoor: {
+          900: '#1f2d3d',
+          800: '#3c4858',
+          400: '#c0ccda',
+          200: '#e0e6ed',
+          100: '#f9fafc',
+        }
+      },
+    },
+    fontFamily: {
+      rale: ['Raleway'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
