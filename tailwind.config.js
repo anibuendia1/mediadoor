0efdd04d0d46866a6c22a43a7d2cfd98194d8ce2
Aniceto <anibuendia3@gmail.com>
2021-12-10 13:22:43 +0100

    Added colors and fonts in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        mediadoor: {
-          900: '#1f2d3d',
-          800: '#3c4858',
-          400: '#c0ccda',
-          200: '#e0e6ed',
-          100: '#f9fafc',
-        }
+        mblack: '#161f1f',
+        myellow: '#f7dc00',
+        myellowbk: '#d6b42b',
+        mpurple: '#d8c0cb',
+        mpurplebk: '#94868d',
+        mgreen: '#aac99d',
+        mgreenbk: '#869c7c',
+        mred: '#c7615f',
+        mredbk: '#985150',
+        mblue: '#85bcc4',
+        mbluebk: '#329e9e',
       },
     },
     fontFamily: {
       rale: ['Raleway'],
+      bebas: ['Bebas Neue']
     },
   },
   variants: {
